6c2a42c46258cc171b93f71e6318455cf213b29d
thilakshids <thilakshids@gmail.com>
2021-08-25 10:31:07 +0530

    added

---
+++ src/Login/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Color, Container, Row } from "react-bootstrap";
+
+const Login =  () => {
+    return(
+   <>
+        <Container>
+            <Row>
+                <Col lg={4} nd={6} sn={12}>
+
+                </Col>
+
+                <Col lg={8} nd={6} sn={12}>
+
+                </Col>
+                </Row>
+                </Container>
+                </>
+                
+    )
+}
