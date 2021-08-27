601667b8fbbf855c303bfaebd329222cd234c659
thilakshids <thilakshids@gmail.com>
2021-08-27 21:27:30 +0530

    add

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -8,6 +8,27 @@ const Login =  () => {
             <Row>
                 <Col lg={4} nd={6} sn={12}>
 
+                <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
                 </Col>
 
                 <Col lg={8} nd={6} sn={12}>
