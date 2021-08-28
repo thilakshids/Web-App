2929c33ee2f048da4458e2696cb2172fff965ca1
thilakshids <thilakshids@gmail.com>
2021-08-28 11:17:16 +0530

    added

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -9,8 +9,8 @@ const Login =  () => {
                 <Col lg={4} nd={6} sn={12}>
 
                 <Form>
-  {/* <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label> */}
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
